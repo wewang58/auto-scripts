f949d32cb0ce23586bde61d9f792b36e2bed13bd
dongboyan <dyan@redhat.com>
2016-03-04 10:11:05 +0800

    no need push

--- pull-images.sh
+++ pull-images.sh
@@ -11,10 +11,6 @@ PYTHON33=(python-33-rhel7 openshift3/python-33-rhel7)
 PYTHON34=(python-34-rhel7 rhscl/python-34-rhel7)
 PHP55=(php-55-rhel7 openshift3/php-55-rhel7)
 PHP56=(php-56-rhel7 rhscl/php-56-rhel7)
-#TOMCAT7=(webserver30-tomcat7-openshift jboss-webserver-3/webserver30-tomcat7-openshift)
-#TOMCAT8=(webserver30-tomcat8-openshift jboss-webserver-3/webserver30-tomcat8-openshift)
-#EAP64=(eap64-openshift jboss-eap-6/eap64-openshift)
-#AMQ62=(amq62-openshift jboss-amq-6/amq62-openshift)
 JENKINS=(jenkins-1-rhel7 openshift3/jenkins-1-rhel7)
 MYSQL55=(mysql-55-rhel7 openshift3/mysql-55-rhel7)
 MYSQL56=(mysql-56-rhel7 rhscl/mysql-56-rhel7)
@@ -22,8 +18,12 @@ MONGO24=(mongodb-24-rhel7 openshift3/mongodb-24-rhel7)
 MONGO26=(mongodb-26-rhel7 rhscl/mongodb-26-rhel7)
 POST92=(postgresql-92-rhel7 openshift3/postgresql-92-rhel7)
 POST94=(postgresql-94-rhel7 rhscl/postgresql-94-rhel7)
+TOMCAT7=(webserver30-tomcat7-openshift jboss-webserver-3/webserver30-tomcat7-openshift)
+TOMCAT8=(webserver30-tomcat8-openshift jboss-webserver-3/webserver30-tomcat8-openshift)
+EAP64=(eap64-openshift jboss-eap-6/eap64-openshift)
+AMQ62=(amq62-openshift jboss-amq-6/amq62-openshift)
 
-IMAGE=(RUBY20 RUBY22 NODEJS PERL516 PERL520 PYTHON27 PYTHON33 PYTHON34 PHP55 PHP56 JENKINS MYSQL55 MYSQL56 MONGO24 MONGO26 POST92 POST94)
+IMAGE=(RUBY20 RUBY22 NODEJS PERL516 PERL520 PYTHON27 PYTHON33 PYTHON34 PHP55 PHP56 JENKINS MYSQL55 MYSQL56 MONGO24 MONGO26 POST92 POST94 TOMCAT7 TOMCAT8 EAP64 AMQ62)
 
 for (( i=0; i<${#IMAGE[@]} ; i++)) ; do
   echo "number:$i"
@@ -38,27 +38,27 @@ for (( i=0; i<${#IMAGE[@]} ; i++)) ; do
 #check image ID
    if [ "$RIMAGE_ID" != "$UIMAGE_ID" ];then
       echo "$NAME image updated, need testing"
-      docker tag -f registry.access.redhat.com/$REG:latest virt-openshift-05.lab.eng.nay.redhat.com:5001/ose-release/$NAME:latest
-      docker push virt-openshift-05.lab.eng.nay.redhat.com:5001/ose-release/$NAME:latest |tee -a push.log
+#      docker tag -f registry.access.redhat.com/$REG:latest virt-openshift-05.lab.eng.nay.redhat.com:5001/ose-release/$NAME:latest
+#      docker push virt-openshift-05.lab.eng.nay.redhat.com:5001/ose-release/$NAME:latest |tee -a push.log
 #check push successful
-      DIFF=`cat push.log |grep sha256 |cut -d ':' -f1,2,3`
-        if [ "$DIFF" != "latest: digest: sha256" ];then 
-            echo "push release $NAME failed"
-        else 
-            echo "push release $NAME successfully"
-        fi
-        rm -f push.log
+#      DIFF=`cat push.log |grep sha256 |cut -d ':' -f1,2,3`
+#        if [ "$DIFF" != "latest: digest: sha256" ];then 
+#            echo "push release $NAME failed"
+#        else 
+#            echo "push release $NAME successfully"
+#        fi
+#        rm -f push.log
 
-      docker tag -f brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/$REG:latest virt-openshift-05.lab.eng.nay.redhat.com:5001/ose-upgrade/$NAME:latest
-      docker push virt-openshift-05.lab.eng.nay.redhat.com:5001/ose-upgrade/$NAME:latest |tee -a push.log
+#      docker tag -f brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/$REG:latest virt-openshift-05.lab.eng.nay.redhat.com:5001/ose-upgrade/$NAME:latest
+#      docker push virt-openshift-05.lab.eng.nay.redhat.com:5001/ose-upgrade/$NAME:latest |tee -a push.log
 #check push successful
-      DIFF=`cat push.log |grep sha256 |cut -d ':' -f1,2,3`
-        if [ "$DIFF" != "latest: digest: sha256" ];then
-           echo "push upgrade $NAME failed"
-        else 
-           echo "push upgrade $NAME successfully"
-        fi 
-        rm -f push.log
+#      DIFF=`cat push.log |grep sha256 |cut -d ':' -f1,2,3`
+#        if [ "$DIFF" != "latest: digest: sha256" ];then
+#           echo "push upgrade $NAME failed"
+#        else 
+#           echo "push upgrade $NAME successfully"
+#        fi 
+#        rm -f push.log
 #delete release images
       docker rmi -f $RIMAGE_ID       
 
